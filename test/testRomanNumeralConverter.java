e64950e77487e795957d3bfa3670b356bb112cd9
Lawrence Davidson <lawrencex2@gmail.com>
2016-03-27 10:40:37 -0400

    refactor testRomanNumeralConverter

--- test/testRomanNumeralConverter.java
+++ test/testRomanNumeralConverter.java
@@ -1,4 +1,5 @@
 import com.company.RomanNumeralConverter;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -7,22 +8,24 @@ import static org.junit.Assert.assertEquals;
  * Created by lawrence on 3/27/16.
  */
 public class testRomanNumeralConverter {
+    private RomanNumeralConverter romanNumeralConverter;
+    @Before
+    public void setUp() {
+        romanNumeralConverter = new RomanNumeralConverter();
+    }
     @Test
     public void testIntegerOneReturnsI() {
-        RomanNumeralConverter romanNumeralConverter = new RomanNumeralConverter();
         assertEquals("I", romanNumeralConverter.convertToRomanNumeral(1));
     }
 
     @Test
     public  void testTestIntegerUpToThreeReturnsCorrectRomanNumeral() {
-        RomanNumeralConverter romanNumeralConverter = new RomanNumeralConverter();
         assertEquals("II", romanNumeralConverter.convertToRomanNumeral(2));
         assertEquals("III", romanNumeralConverter.convertToRomanNumeral(3));
     }
 
     @Test
     public void testReturnOfSingleRomanNumeralsVXLCDM() {
-        RomanNumeralConverter romanNumeralConverter = new RomanNumeralConverter();
         assertEquals("V", romanNumeralConverter.convertToRomanNumeral(5));
         assertEquals("X", romanNumeralConverter.convertToRomanNumeral(10));
         assertEquals("L", romanNumeralConverter.convertToRomanNumeral(50));
