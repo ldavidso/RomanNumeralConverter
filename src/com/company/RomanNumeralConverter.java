1f82253d03a4a5028de48c8dbed60ac7e1a97368
Lawrence Davidson <lawrencex2@gmail.com>
2016-03-27 12:59:12 -0400

    add comments to code

--- src/com/company/RomanNumeralConverter.java
+++ src/com/company/RomanNumeralConverter.java
@@ -9,6 +9,9 @@ public class RomanNumeralConverter {
     private static Integer[] romanNumeralValues = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 
     public String convert(Integer userInput) {
+        // Checks minimum and maximum number that can be converted to roman numeral
+        // all rules to convert from arabic number to roman numeral are part of the order of the two
+        // arrays, romanNumerals and romanNumeralValues.
         if (userInput > 3999 || userInput <= 0) {
             return "Please use a number between 1 - 3999";
         }
@@ -28,12 +31,14 @@ public class RomanNumeralConverter {
     }
 
     public String convert(String userInput) {
+        // used for validation of roman numeral to arabic number.
         String[] invalidSequences = {"IIII", "XXXX", "CCCC", "MMMM", "VV", "LL", "DD"};
         String[] validForI = {"X", "V", "I"};
         String[] validForX = {"C", "L", "X", "V", "I"};
         boolean validRomanNumeral;
         String romanNumeral;
 
+        // Check for over 3 of I, X, C, or M and more than one of V, L, and D
         for (String invalidSequence: invalidSequences) {
             if(userInput.contains(invalidSequence)) {
                 return "Invalid roman numeral!";
@@ -46,7 +51,9 @@ public class RomanNumeralConverter {
         while (userInputLength > 0) {
             for (int i = 0; i < romanNumerals.length; i++) {
                 int currentRomanNumeralLength = romanNumerals[i].length();
+                // first checks to see if userInput can substring() based on current length of userInput and current Roman Numeral being tested.
                 if (userInputLength >= currentRomanNumeralLength && userInput.substring(0,currentRomanNumeralLength).equals(romanNumerals[i])) {
+                    // used to validate subtraction rule.
                     if (userInputLength  > 1) {
                         validRomanNumeral = false;
                         switch (romanNumerals[i]) {
