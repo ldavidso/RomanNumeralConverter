1891354c89f5ec2c23fa8f95286b6d33d93f6834
Lawrence Davidson <lawrencex2@gmail.com>
2016-04-04 11:45:52 -0400

    refactor: remove unused import

--- test/testRomanNumeralConverter.java
+++ test/testRomanNumeralConverter.java
@@ -3,7 +3,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 /**
  * Created by lawrence on 3/27/16.
